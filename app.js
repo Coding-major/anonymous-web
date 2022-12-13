90c0940b74820da3f3836f4914381f551f3b985f
Shehu Mustapha <shehumustapham@gmail.com>
2022-12-13 17:03:55 +0100

    completed the md5 hashing authentication

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ const mongoose = require("mongoose");
 const md5 = require("md5")
 
 
+
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyparser.urlencoded({extended: true}));
 
-mongoose.connect("mongodb://localhost:27017/userDB")
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema ({
     email: String,
@@ -36,20 +37,22 @@ app.get("/register", (req, res) => {
     res.render("register")
 });
 
-app.post("/register", (req, res) => {
-   const newUser = new User(
-    {
-        email: req.body.username,
-        password: md5(req.body.password)
-    }
-   )
-   newUser.save(function(err) {
-    if (err) {
-        console.log(err)
-    } else {
-        res.render("secrets")
-    }
-   })
+app.post("/register", (req, res) => { 
+    const newUSer = new User(
+        {
+            email: req.body.username,
+            password: md5(req.body.password)
+        }
+    )
+
+    newUSer.save(function(err) {
+        if (err) {
+            console.log(err)
+        } else {
+            res.render("secrets")
+        }
+    })
+   
 })
 
 app.post("/login", (req, res) => {
