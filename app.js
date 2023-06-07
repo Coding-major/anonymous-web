fabd43c55276aef5b2ff326bd66e54e92e9fb424
Shehu Mustapha <shehumustapham@gmail.com>
2023-06-07 23:23:49 +0100

    changed mongo connect from local to international

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session")
+const passport = require("passport")  
+const passportLocalMongoose = require("passport-local-mongoose"); 
 
 
 
@@ -13,18 +14,41 @@ const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
 
-mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true});
+app.use(session({
+    secret: "my name is mustey",
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session())
+
+
+//mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true});
+const connectDB = (url) => {
+    return mongoose.connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+}
 
 const userSchema = new mongoose.Schema ({
     email: String,
     password: String
 })   
-
+ 
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema)
 
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser())
 
 app.get("/", (req, res) => {
     res.render("home")
@@ -38,45 +62,89 @@ app.get("/register", (req, res) => {
     res.render("register")
 });
 
+app.get("/secrets", function(req, res) {
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        res.redirect("/login")
+    }
+})
+
+app.get("/submit", (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render("submit");
+    } else {
+        res.redirect("/login")
+    }
+})
+
+app.get("/logout", (req, res, next) => {
+    req.logout(function(err) {
+        if (err) {
+            return next(err);
+        }
+        res.redirect("/")
+    });
+})
+
 app.post("/register", (req, res) => {
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const newUSer = new User(
-            {
-                email: req.body.username,
-                password: hash
-            }
-        )
-        newUSer.save(function(err) {
-            if (err) {
-                console.log(err)
-            } else {
-                res.render("secrets")
-            }
+  User.register({username: req.body.username}, req.body.password, (err, user) => {
+    if (err) {
+        console.log(err);
+        res.redirect("/register")
+    } else {
+        passport.authenticate("local")(req, res, function() {
+            res.redirect("/secrets")
         })
-       
-    })
+    }
+  })
+})
+
+app.post("/submit", async (req, res) => {
+    try {
+        const posted = await User.create(req.body.name)
+        res.status(201).json({posted})
+        console.log(posted)
+    } catch (error) {
+        console.log(error);
+    }
 })
 
 app.post("/login", (req, res) => {
-    const username = req.body.username
-    const password = req.body.password;
+    const user = new User(
+        {
+            username: req.body.username,
+            password: req.body.password
+        }
+    )
 
-    User.findOne({email: username}, function(err, foundUser) {
+
+    req.login(user, function(err) {
         if (err) {
             console.log(err);
         } else {
-            if (foundUser) {
-                bcrypt.compare(password, foundUser.password, function(err, result) {
-                    if (result === true) {
-                        res.render("secrets")
-                    }
-                })
-            }
+            passport.authenticate("local")(req, res, () => {
+                res.redirect("/secrets")
+            })
         }
     })
 })
 
 
-app.listen(3000, () => {
-    console.log("server start at port 3000");
-})
+// app.listen(3000, () => {
+//     console.log("server start at port 3000");
+// })
+
+const port = 3000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`server is listening to port ${port}.....`))
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+start()
+
